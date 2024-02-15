3f79496847bd36297284a5aa14fd617b8a837262
abdulladone94 <abdullaf@booleanlabs.biz>
2024-02-15 20:52:13 +0530

    completed get all currency names

--- src/index.js
+++ src/index.js
@@ -8,4 +8,16 @@ const port = 3001;
 app.use(express.json());
 app.use(cors());
 
+app.get('/getAllCurrencies', async (req, res) => {
+  const currencyNamesFromOpenExchangeRates =
+    'https://openexchangerates.org/api/currencies.json?app_id=633118020ef2403db7fb8ab4ce0927f2';
+
+  try {
+    const currencyNameRes = await axios.get(currencyNamesFromOpenExchangeRates);
+    return res.json(currencyNameRes.data);
+  } catch (error) {
+    console.error(error);
+  }
+});
+
 app.listen(port, () => console.log(`Server up and running on port ${port}`));
